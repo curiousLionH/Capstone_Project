0497436265a9914db4dfaa4a3d05571cb96da47b
Jeong_Junhwan <zxxzx1515@naver.com>
2022-05-06 18:15:26 +0900

    faceID+alcohol
    .

--- main.py
+++ main.py
@@ -30,9 +30,15 @@ class WindowClass(QMainWindow, form_class):
 
         # 유저 얼굴 확인 됐으면 True, 아니면 False
         self.identify_user_token = 0
-        self.user_dic = {0:"jiwon", 1:"junhwan"}
+        self.user_dic = {0: "jiwon", 1: "junhwan"}
         self.cap = cv2.VideoCapture(0)
-        #
+
+        # 알코올 패스 함?
+        self.alcohol_pass = False
+        self.alcohol_restart = False
+
+        # 블루투스통신값(음주측정)
+        self.alcohol_value = None
 
         # 얘가 음주측정 안내, 성공 실패, 얼굴인식 다 여기서 멘트 안내.
         # 가이드 라벨 invisible
@@ -42,15 +48,8 @@ class WindowClass(QMainWindow, form_class):
         self.camera_start_button.clicked.connect(self.camera_start)
         self.camera_start_button.clicked.connect(lambda: self.faceID_start("jiwon"))
 
-        # 얼굴인식 완료됐다고 치는 버튼
-        self.temp_button_1.clicked.connect(self.guide_alcohol_check)
-
-        # 얼굴 재인식 요청 버튼
-        self.temp_button_2.clicked.connect(self.guide_face_check_again)
-
         self.time = time()
         self.Eye_Track = Align_Depth_Eye_Track()
-
 
     # 카메라 보여주기
     def camera_show(self):
@@ -96,6 +95,11 @@ class WindowClass(QMainWindow, form_class):
 
     def faceID_start(self, user):
         th = threading.Thread(target=self.faceID, args=(user,))
+        th.start()
+        print("started..")
+
+    def faceID_alcohol_start(self, user):
+        th = threading.Thread(target=self.faceID_alcohol, args=(user,))
         th.start()
         print("started..")
 
@@ -177,6 +181,87 @@ class WindowClass(QMainWindow, form_class):
                         else:
                             print("hello jiwon")
                             return
+
+                process_this_frame = not process_this_frame
+            except:
+                pass
+
+        self.camera_start_button.setText("음주 측정 시작")
+        self.user_guide_label.setText("음주 측정을 시작해주세요")
+        self.camera_start_button.clicked.disconnect(self.camera_start)
+        self.camera_start_button.clicked.disconnect(lambda: self.faceID_start("jiwon"))
+
+        self.camera_start_button.clicked.connect(
+            lambda: self.faceID_alcohol_start("jiwon")
+        )
+
+    def faceID_alcohol(self, user="jiwon"):
+        print("let's start faceID")
+
+        video_capture = self.cap
+
+        path = "%s.jpg" % (user)
+        img = face_recognition.load_image_file(path)
+        face_encoding = face_recognition.face_encodings(img)[0]
+
+        known_face_encodings = [face_encoding]
+        known_face_names = [user]
+
+        face_locations = []
+        face_encodings = []
+        face_names = []
+        process_this_frame = True
+        while not self.alcohol_pass:
+            try:
+                # Grab a single frame of video
+                ret, frame = video_capture.read()
+                print(f"video_capture ret = {ret}")
+                self.face_frame = frame
+
+                # Resize frame of video to 1/4 size for faster face recognition processing
+                small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+                # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+                rgb_small_frame = small_frame[:, :, ::-1]
+
+                # Only process every other frame of video to save time
+                if process_this_frame:
+                    # Find all the faces and face encodings in the current frame of video
+                    face_locations = face_recognition.face_locations(rgb_small_frame)
+                    face_encodings = face_recognition.face_encodings(
+                        rgb_small_frame, face_locations
+                    )
+
+                    face_names = []
+                    for face_encoding in face_encodings:
+                        # See if the face is a match for the known face(s)
+                        matches = face_recognition.compare_faces(
+                            known_face_encodings, face_encoding
+                        )
+                        name = "Unknown"
+
+                        # # If a match was found in known_face_encodings, just use the first one.
+                        # if True in matches:
+                        #     first_match_index = matches.index(True)
+                        #     name = known_face_names[first_match_index]
+
+                        # Or instead, use the known face with the smallest distance to the new face
+                        face_distances = face_recognition.face_distance(
+                            known_face_encodings, face_encoding
+                        )
+                        best_match_index = np.argmin(face_distances)
+                        if matches[best_match_index]:
+                            name = known_face_names[best_match_index]
+
+                        face_names.append(name)
+
+                        if name == "Unknown":
+                            print("who are you")
+                            self.alcohol_restart = True
+                        else:
+                            print("hello jiwon")
+                            if self.alcohol_value:  # 음주 통과
+                                self.alcohol_pass = True
 
                 process_this_frame = not process_this_frame
             except:
